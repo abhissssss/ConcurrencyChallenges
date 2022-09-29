c1b2b1d77ad9d1f570f3efa044c9de00622ab703
Abhishek Singh <abhisingh352000@gmail.com>
2022-09-29 14:58:55 +0300

    Implemented try lock() in the bankAccount class

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,3 +1,4 @@
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -7,61 +8,54 @@ public class BankAccount {
     private double balance;
     private Lock reentrantLock;
 
-    public BankAccount(String accountNumber, double initialBalance ) {
+    public BankAccount(String accountNumber, double initialBalance) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
-        this.reentrantLock= new ReentrantLock();
+        this.reentrantLock = new ReentrantLock();
     }
 
 
-    //    public synchronized double deposit(double amount){
-//
-//        if (amount<=0){
-//            System.out.println("Cannot add such amount");
-//        }
-//        return this.balance+=amount;
-//    }
     public double deposit(double amount) {
-        reentrantLock.lock();
-          try{
-              if (amount <= 0) {
-                  System.out.println("Cannot add such amount");
-              }
-              return this.balance += amount;
-          }finally {
-              reentrantLock.unlock();
-          }
-
-        }
-
-//    public synchronized double withdraw(double amount) {
-//        if (amount > balance) {
-//            System.out.println("Insufficient balance");
-//        }
-//        return this.balance -= amount;
-//
-//    }
-
-    public  double withdraw(double amount) {
-reentrantLock.lock();
         try {
-            if (amount > balance) {
-                System.out.println("Insufficient balance");
+            if (reentrantLock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance += amount;
+                } finally {
+                    reentrantLock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock too");
             }
-            return this.balance -= amount;
-        } finally {
-            reentrantLock.unlock();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        return amount;
 
+    }
 
+    public double withdraw(double amount) {
+        try {
+            if (reentrantLock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance -= amount;
+                } finally {
+                    reentrantLock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        return amount;
+    }
 
-    public  String getAccountNumber() {
+    public String getAccountNumber() {
 
         return accountNumber;
     }
 
-    public  void printAccountNumber(){
-        System.out.println("Account number is: " +this.accountNumber);
+    public void printAccountNumber() {
+        System.out.println("Account number is: " + this.accountNumber);
     }
 }
