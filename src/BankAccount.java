1f12acd4cac7b2c4570cda70f65ba04768cac00d
Abhishek Singh <abhisingh352000@gmail.com>
2022-09-29 18:01:22 +0300

    fixed challenge using wait() and notify()

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -6,26 +6,57 @@ public class BankAccount {
 
     private final String accountNumber;
     private double balance;
+    private Lock lock;
 
 
     public BankAccount(String accountNumber, double initialBalance) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
+        this.lock = new ReentrantLock();
     }
 
 
     public double deposit(double amount) {
-    if (amount<=0){
-        System.out.println("Cannot add this amount");
-    }
-      return this.balance+=amount;
+        boolean status = false;
+        try {
+            if (lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance += amount;
+                    status = true;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Transaction status = " + status);
+        return amount;
+
     }
 
     public double withdraw(double amount) {
-if (amount>balance){
-    System.out.println("Insufficient funds cannot withdraw");
-} return this.balance-=amount;
+        boolean status = false;
+        try {
+            if (lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                try {
+                    balance -= amount;
+                    status = true;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("Could not get the lock");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Transaction status =" + status);
+        return amount;
     }
+
     public String getAccountNumber() {
 
         return accountNumber;
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,18 @@
-import java.util.concurrent.locks.ReentrantLock;
+
 
 public class Main {
     public static void main(String[] args) {
-        ReentrantLock lock = new ReentrantLock(true);
-BankAccount myAccount = new BankAccount("9870214102" , 1000 );
-new Thread(() -> {
-    lock.lock();
-    try{
-        System.out.println("The new balance after deposit is: " + myAccount.deposit(2010.98));
-        System.out.println("The new balance after withdrawal is: " + myAccount.withdraw(50.65));
-    }finally {
-        lock.unlock();
-    }
-}).start();
 
-new Thread(() -> {
-    lock.lock();
-    try {
+        MyBankAccount myAccount = new MyBankAccount("9870214102", 1000);
+        new Thread(() -> {
+            System.out.println("The new balance after deposit is: " + myAccount.deposit(201.98));
+            System.out.println("The new balance after withdrawal is: " + myAccount.withdraw(50.65));
+        }).start();
 
-        System.out.println("New balance after deposit is: " + myAccount.deposit(573.76));
-        System.out.println("New balance after withdrawal is: " + myAccount.withdraw(213.91));
-    }finally {
-        lock.unlock();
-    }
-}).start();
+        new Thread(() -> {
+
+            System.out.println("New balance after deposit is: " + myAccount.deposit(573.76));
+            System.out.println("New balance after withdrawal is: " + myAccount.withdraw(213.91));
+        }).start();
     }
 }
